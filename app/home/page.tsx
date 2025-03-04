d54ca50dde1d99b92839dc2d5ca33ffe601fb955
SaimonNeupane <Saimonbro00007@gmail.com>
2025-03-04 09:19:31 +0545

    layout for hero section

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import ServicesSection from '../../components/home/ServicesSection';
+import ServicesSection from "../../components/home/ServicesSection";
+import HeroSection from "@/components/home/HeroSection";
 
-function home () {
+function home() {
   return (
     <div className="min-h-screen bg-gray-50">
+      <HeroSection />
       <ServicesSection />
-      
-     
     </div>
-  )
+  );
 }
 
 export default home;
--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 function HeroSection() {
-  return <div>HeroSection</div>;
+  return (
+    <div>
+      <div className="h-[80vh] md:h-[60vh] bg-[#ef4444]/20 m-4 md:m-9 rounded-4xl md:rounded-none md:rounded-tr-4xl flex ">
+        <div className="md:w-[50%] p-5 h-[100%] flex flex-col">
+          <div className="text-3xl text-[#443d3d] font-bold pt-96 md:pt-48 w-full flex items-center ">
+            <div>
+              Transform your vision into a Digital Reality with the best IT
+              company in Nepal
+            </div>
+          </div>
+          <div className="flex gap-5 pt-20 ">
+            <div>
+              <button className="border-red-800 h-[30px] w-[170px] border-2 flex justify-center items-center cursor-pointer rounded-4xl">
+                Let's get Started
+              </button>
+            </div>
+            <div>
+              <button className="border-red-800 h-[30px] w-[170px] border-2 flex justify-center items-center cursor-pointer rounded-4xl">
+                Discover our work
+              </button>
+            </div>
+          </div>
+        </div>
+        <div>images here</div>
+      </div>
+    </div>
+  );
 }
 
 export default HeroSection;
