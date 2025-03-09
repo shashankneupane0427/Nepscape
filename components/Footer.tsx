ac503352055ae4dc794cffac67427dfc5811e78d
Surya Shashank Neupane <suryaneupane0427@gmail.com>
2025-03-09 19:11:04 +0545

    fix: update styles in the Footer component for better responsiveness

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,147 +13,135 @@ import {
 
 const Footer: React.FC = () => {
   return (
-  
-      <div>
+    <div>
       <footer className="relative py-12 pt-0 pb-0 mt-0 text-black">
-          {/* Connect Section */}
-      <div
-        className="block mb-12 overflow-hidden text-start bg-gray-50 "
-        style={{
-          // backgroundImage: "url(https://softbenz.com/static/site-assets/images/cta_bg1.png)",
-          backgroundPosition: "50% 50%",
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          boxSizing: "border-box",
-          color: "rgb(0, 0, 0)",
-          fontFamily: "Poppins, sans-serif",
-          fontSize: "16px",
-          fontWeight: "400",
-          height: "285.438px",
-          lineHeight: "30px",
-          paddingBottom: "65.35px",
-          paddingTop: "65.35px",
-          textSizeAdjust: "100%",
-          unicodeBidi: "isolate",
-          width: "100%",
-          WebkitTapHighlightColor: "rgba(0, 0, 0, 0)",
-       
-          overflowX: "hidden",
-          overflowY: "hidden",
-        }}
-      >
-        <div className="container flex flex-col items-center justify-center px-0 mx-auto md:flex-row">
-          {/* Left Section */}
-          <div className="md:w-2/3">
-            <div className="border-l-4 border-[#a46ede] pl-4">
-              <h2 className="text-4xl font-bold leading-tight text-black">
-                Let&apos;s <span className="text-black">connect</span>
-                <span className="text-[#a46ede]"> and turn your vision into reality.</span>
-              </h2>
+        {/* Connect Section */}
+        <div className="block overflow-hidden text-start bg-gray-50 py-10 md:py-12 lg:py-16">
+          <div className="container px-4 mx-auto">
+            <div className="flex flex-col items-center justify-center md:flex-row md:justify-between">
+              {/* Left Section */}
+              <div className="md:w-2/3 w-full text-center md:text-left mb-8 md:mb-0">
+                <div className="border-l-4 border-[#a46ede] pl-4">
+                  <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold leading-tight text-black">
+                    Let&apos;s <span className="text-black">connect</span>
+                    <span className="text-[#a46ede]"> and turn your vision into reality.</span>
+                  </h2>
+                </div>
+                <p className="mt-4 text-base sm:text-lg text-gray-600">
+                  We are available from 9:00 AM to 6:00 PM, Monday to Friday.
+                </p>
+              </div>
+
+              {/* Right Section */}
+              <div className="flex flex-col items-center md:items-end">
+                <p className="text-sm font-semibold tracking-wide text-gray-700">REACH OUT NOW!</p>
+
+                {/* Phone number with WhatsApp link */}
+                <a
+                  href="https://wa.me/9862973810"
+                  className="text-2xl sm:text-3xl md:text-4xl font-bold text-[#a46ede] my-2 cursor-pointer"
+                  target="_blank"
+                >
+                  9862973810
+                </a>
+
+                <a
+                  href="/contactus"
+                  className="bg-[#a46ede] text-white px-4 py-2 sm:px-6 sm:py-3 rounded-full hover:bg-white hover:text-[#8b5cf6] border border-[#a46ede] transition duration-300 font-semibold text-sm sm:text-base"
+                >
+                  Let's Start Conversation
+                </a>
+              </div>
             </div>
-            <p className="mt-4 text-lg text-gray-600">
-              We are available from 9:00 AM to 6:00 PM, Monday to Friday.
-            </p>
-          </div>
-
-          {/* Right Section */}
-          <div className="flex flex-col items-center justify-center mt-8 md:items-end md:mt-0">
-            <p className="text-sm font-semibold tracking-wide text-gray-700">REACH OUT NOW!</p>
-
-            {/* Phone number with WhatsApp link */}
-            <a
-              href="https://wa.me/9862973810"
-              className="text-4xl font-bold text-[#a46ede] my-2 cursor-pointer"
-              target="_blank"
-            >
-              9862973810
-            </a>
-
-            <a
-              href="/contactus"
-              className="bg-[#a46ede] text-white px-6 py-3 rounded-full hover:bg-white hover:text-[#8b5cf6] border border-[#a46ede] transition duration-300 font-semibold"
-            >
-              Let's Start Conversation
-            </a>
           </div>
         </div>
-      </div>
-      <div className="container grid grid-cols-1 gap-12 mx-auto sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {/* Development Services Section */}
-        <div className="pl-8">
-          <h4 className="mb-4 text-xl font-semibold">Development Services</h4>
-          <ul>
-            <li className="mb-2"><Link href="#" className="text-[#5d605d] hover:text-[#7d2fd0]">Website Development</Link></li>
-            <li className="mb-2"><Link href="#" className="text-[#5d605d] hover:text-[#7d2fd0]">App Development</Link></li>
-            <li className="mb-2"><Link href="#" className="text-[#5d605d] hover:text-[#7d2fd0]">System/Software Development</Link></li>
-            <li className="mb-2"><Link href="#" className="text-[#5d605d] hover:text-[#7d2fd0]">UI/UX</Link></li>
-          </ul>
+
+        {/* Links Section */}
+        <div className="container px-4 mx-auto py-10 md:py-12">
+          <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            {/* Development Services Section */}
+            <div className="text-center sm:text-left">
+              <h4 className="mb-4 text-xl font-semibold">Development Services</h4>
+              <ul>
+                <li className="mb-2"><Link href="#" className="text-[#5d605d] hover:text-[#7d2fd0]">Website Development</Link></li>
+                <li className="mb-2"><Link href="#" className="text-[#5d605d] hover:text-[#7d2fd0]">App Development</Link></li>
+                <li className="mb-2"><Link href="#" className="text-[#5d605d] hover:text-[#7d2fd0]">System/Software Development</Link></li>
+                <li className="mb-2"><Link href="#" className="text-[#5d605d] hover:text-[#7d2fd0]">UI/UX</Link></li>
+              </ul>
+            </div>
+
+            {/* Marketing Services Section */}
+            <div className="text-center sm:text-left">
+              <h4 className="mb-4 text-xl font-semibold">Marketing Services</h4>
+              <ul>
+                <li className="mb-2"><Link href="#" className="text-[#5d605d] hover:text-[#7d2fd0]">Search Engine Optimization (SEO)</Link></li>
+                <li className="mb-2"><Link href="#" className="text-[#5d605d] hover:text-[#7d2fd0]">Social Media Marketing (SMM)</Link></li>
+                <li className="mb-2"><Link href="#" className="text-[#5d605d] hover:text-[#7d2fd0]">Graphic Design</Link></li>
+                <li className="mb-2"><Link href="#" className="text-[#5d605d] hover:text-[#7d2fd0]">Content Writing</Link></li>
+                <li className="mb-2"><Link href="#" className="text-[#5d605d] hover:text-[#7d2fd0]">Pay Per Click</Link></li>
+                <li className="mb-2"><Link href="#" className="text-[#5d605d] hover:text-[#7d2fd0]">Digital Marketing</Link></li>
+              </ul>
+            </div>
+
+            {/* Quick Links Section */}
+            <div className="text-center sm:text-left">
+              <h4 className="mb-4 text-xl font-semibold">Quick Links</h4>
+              <ul>
+                <li className="mb-2"><Link href="/about" className="text-[#5d605d] hover:text-[#7d2fd0]">About Us</Link></li>
+                <li className="mb-2"><Link href="/services" className="text-[#5d605d] hover:text-[#7d2fd0]">Our Services</Link></li>
+                <li className="mb-2"><Link href="/contact" className="text-[#5d605d] hover:text-[#7d2fd0]">Contact Us</Link></li>
+                <li className="mb-2"><Link href="/blog" className="text-[#5d605d] hover:text-[#7d2fd0]">Blog</Link></li>
+                <li className="mb-2"><Link href="/career" className="text-[#5d605d] hover:text-[#7d2fd0]">Career</Link></li>
+                <li className="mb-2"><Link href="/our-work" className="text-[#5d605d] hover:text-[#7d2fd0]">Our Work</Link></li>
+              </ul>
+            </div>
+
+            {/* Connect With Us Section */}
+            <div className="text-center sm:text-left">
+              <h4 className="mb-6 text-xl font-semibold">Connect with Us</h4>
+              <ul className="space-y-6">
+                <li className="flex items-center text-lg text-[#5d605d] justify-center sm:justify-start">
+                  <FaPhoneAlt className="mr-4 text-[#7d2fd0] text-2xl" />
+                  <a href="tel:+9862973810" className="text-[#5d605d] hover:text-[#7d2fd0] hover:underline">9862973810</a>
+                </li>
+                <li className="flex items-center text-lg text-[#5d605d] justify-center sm:justify-start">
+                  <FaMapMarkerAlt className="mr-4 text-[#7d2fd0] text-2xl" /> Putalisadak, Kathmandu
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
 
-        {/* Marketing Services Section */}
-        <div>
-          <h4 className="mb-4 text-xl font-semibold">Marketing Services</h4>
-          <ul>
-            <li className="mb-2"><Link href="#" className="text-[#5d605d] hover:text-[#7d2fd0]">Search Engine Optimization (SEO)</Link></li>
-            <li className="mb-2"><Link href="#" className="text-[#5d605d] hover:text-[#7d2fd0]">Social Media Marketing (SMM)</Link></li>
-            <li className="mb-2"><Link href="#" className="text-[#5d605d] hover:text-[#7d2fd0]">Graphic Design</Link></li>
-            <li className="mb-2"><Link href="#" className="text-[#5d605d] hover:text-[#7d2fd0]">Content Writing</Link></li>
-            <li className="mb-2"><Link href="#" className="text-[#5d605d] hover:text-[#7d2fd0]">Pay Per Click</Link></li>
-            <li className="mb-2"><Link href="#" className="text-[#5d605d] hover:text-[#7d2fd0]">Digital Marketing</Link></li>
-          </ul>
+        {/* Bottom Links Section */}
+        <div className="border-t border-[#7d2fd0] py-6 text-center text-sm text-black">
+          <div className="container px-4 mx-auto">
+            <div className="flex flex-col sm:flex-row justify-center items-center space-y-2 sm:space-y-0 sm:space-x-4">
+              <Link href="/FAQ" className="hover:text-[#7d2fd0]">FAQ</Link>
+              <span className="hidden sm:inline mx-2">|</span>
+              <Link href="/terms" className="hover:text-[#7d2fd0]">Terms & Conditions</Link>
+              <span className="hidden sm:inline mx-2">|</span>
+              <Link href="/privacy" className="hover:text-[#7d2fd0]">Privacy Policy</Link>
+            </div>
+          </div>
         </div>
-
-        {/* Quick Links Section */}
-        <div className="pl-5">
-          <h4 className="mb-4 text-xl font-semibold">Quick Links</h4>
-          <ul>
-            <li className="mb-2"><Link href="/about" className="text-[#5d605d] hover:text-[#7d2fd0]">About Us</Link></li>
-            <li className="mb-2"><Link href="/services" className="text-[#5d605d] hover:text-[#7d2fd0]">Our Services</Link></li>
-            <li className="mb-2"><Link href="/contact" className="text-[#5d605d] hover:text-[#7d2fd0]">Contact Us</Link></li>
-            <li className="mb-2"><Link href="/blog" className="text-[#5d605d] hover:text-[#7d2fd0]">Blog</Link></li>
-            <li className="mb-2"><Link href="/career" className="text-[#5d605d] hover:text-[#7d2fd0]">Career</Link></li>
-            <li className="mb-2"><Link href="/our-work" className="text-[#5d605d] hover:text-[#7d2fd0]">Our Work</Link></li>
-          </ul>
-        </div>
-
-        {/* Connect With Us Section */}
-        <div className="text-black">
-          <h4 className="mb-6 text-xl font-semibold text-center sm:text-left">Connect with Us</h4>
-          <ul className="space-y-6">
-            <li className="flex items-center text-lg text-[#5d605d] "><FaPhoneAlt className="mr-4   text-[#7d2fd0] text-2xl" />  <a href="tel:+9801848492" className="text-[#5d605d] hover:text-[#7d2fd0] hover:underline">9862973810</a></li>
-            {/* <li className="flex items-center text-lg"><FaEnvelope className="mr-4   text-[#7d2fd0] text-2xl" />  <a href="mailto:sales@Nepscape.com" className="text-[#5d605d] hover:text-[#7d2fd0] hover:underline">sales@Nepscape.com</a></li> */}
-            <li className="flex items-center text-lg text-[#5d605d] "><FaMapMarkerAlt className="mr-4   text-[#7d2fd0] text-2xl" /> Putalisadak, Kathmandu</li>
-          </ul>
-        </div>
-      </div>
-
-      {/* Bottom Section */}
-      <div className="border-t border-[#7d2fd0] mt-12 pt-6 text-center text-sm text-black">
-        <div className="flex justify-center space-x-6">
-          <Link href="/FAQ" className="hover:text-[#7d2fd0]">FAQ</Link>
-          <span className="mx-2">|</span>
-          <Link href="/terms" className="hover:text-[#7d2fd0]">Terms & Conditions</Link>
-          <span className="mx-2">|</span>
-          <Link href="/privacy" className="hover:text-[#7d2fd0]">Privacy Policy</Link>
-      
-        </div>
-      </div>
-    </footer> 
+      </footer>
         
-    {/* Social Media Section */}
-    <div className="bg-[#7d2fd0] py-4 mt-6 text-center text-white flex justify-between items-center px-6">
-    <span className="text-lg text-white">© 2025 Softbenz Infosys. All Rights Reserved.</span>
-    <div className="flex space-x-4">
-
-      <a href="https://wa.me/9812316018" target="_blank" className="text-2xl text-white"><FaWhatsapp /></a>
-      <a href="#" target="_blank" className="text-2xl text-white"><FaFacebook /></a>
-      <a href="#" target="_blank" className="text-2xl text-white"><FaInstagram /></a>
-      <a href="#" target="_blank" className="text-2xl text-white"><FaLinkedin /></a>
-      <a href="#" target="_blank" className="text-2xl text-white"><FaTwitter /></a>
-      <a href="#" target="_blank" className="text-2xl text-white"><FaYoutube /></a>
-
-    </div>
+      {/* Social Media Section */}
+      <div className="bg-[#7d2fd0] py-4 text-white">
+        <div className="container px-4 mx-auto">
+          <div className="flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
+            <span className="text-sm sm:text-base md:text-lg text-white text-center md:text-left">© 2025 Softbenz Infosys. All Rights Reserved.</span>
+            <div className="flex space-x-4 justify-center">
+              <a href="https://wa.me/9812316018" target="_blank" className="text-xl sm:text-2xl text-white hover:text-gray-200"><FaWhatsapp /></a>
+              <a href="#" target="_blank" className="text-xl sm:text-2xl text-white hover:text-gray-200"><FaFacebook /></a>
+              <a href="#" target="_blank" className="text-xl sm:text-2xl text-white hover:text-gray-200"><FaInstagram /></a>
+              <a href="#" target="_blank" className="text-xl sm:text-2xl text-white hover:text-gray-200"><FaLinkedin /></a>
+              <a href="#" target="_blank" className="text-xl sm:text-2xl text-white hover:text-gray-200"><FaTwitter /></a>
+              <a href="#" target="_blank" className="text-xl sm:text-2xl text-white hover:text-gray-200"><FaYoutube /></a>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
   );
 };
