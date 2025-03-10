259dca6bd434c90133a6586fec3b5338a394ae9f
Surya Shashank Neupane <suryaneupane0427@gmail.com>
2025-03-10 18:27:54 +0545

    feat: restructure home page to include new sections and update component exports for improved layout

--- next.config.ts
+++ next.config.ts
@@ -4,16 +4,7 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // Disables ESLint during builds
   },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/home',
-        permanent: true, // Set to true if you want a 301 permanent redirect
-      },
 
-    ];
-  },
 };
 
 export default nextConfig;
